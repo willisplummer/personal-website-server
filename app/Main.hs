6fc77ef9725bba795de0fe798610997ba838fb34
willis plummer <willisplummer@gmail.com>
2020-03-07 03:29:01 -0500

    postgres

--- app/Main.hs
+++ app/Main.hs
@@ -5,34 +5,42 @@
 module Main where
 
 import           API
-import           Models
 import           Types
 
 import           Control.Monad.Except
 import           Control.Monad.Logger
 import           Control.Monad.Reader
 import           Data.Either
-import           Database.Persist.Sqlite
+import           Database.Persist.Postgresql
+
 import           Network.Wai.Handler.Warp  (run)
 import           Servant
 
+
 import qualified Data.Yaml as Y
 import qualified Data.Map.Strict as Map
+import Web.Heroku.Persist.Postgresql
 
 main :: IO ()
-main = runStdoutLoggingT . withSqliteConn ":memory:" $ \sqlite -> do
-  flip runSqlConn sqlite $ runMigration migrateAll
+main = do
+  pgConf                              <- postgresConf 5
+
   eBooks <- liftIO $ Y.decodeFileEither "./data/books.yml"
   eWritingLinks <- liftIO $ Y.decodeFileEither "./data/writing-links.yml"
-  
+
   let
+    connString = pgConnStr pgConf
+
     parsedBooks :: Map.Map String [Book]
     parsedBooks = fromRight Map.empty eBooks
+
     parsedWritingLinks :: Map.Map String [WritingLink]
     parsedWritingLinks = fromRight Map.empty eWritingLinks
-    appState = AppState { sql = sqlite, readingList = parsedBooks, writingLinks = parsedWritingLinks }
-  liftIO $ putStrLn $ show parsedBooks 
-  lift . run 8080 . serve api $ hoistServer api (runH appState) routes
 
-  where
-    runH appState = Handler . flip runReaderT appState
+  runStdoutLoggingT $ withPostgresqlConn connString $ \db -> do
+    let
+      appState = AppState { sql = db, readingList = parsedBooks, writingLinks = parsedWritingLinks }
+      runH s = Handler . flip runReaderT s
+
+    liftIO $ putStrLn $ show parsedBooks
+    lift . run 8080 . serve api $ hoistServer api (runH appState) routes
