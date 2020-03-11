ebd7710656528e7c7894ed2bad65c53131feee34
willis plummer <willisplummer@gmail.com>
2020-03-11 09:03:49 -0400

    working ghcide

--- shell.nix
+++ shell.nix
@@ -1,14 +1,41 @@
 let
   pkgs = import <nixpkgs> {};
 
-  ghcVersion = "ghc865";
+  haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
+    overrides = self: super: {
+      # If you need a particular version of a package instead
+      # of the default that nixpkgs provides, specify it here.
+    };
+  };
 
-  # haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
-  #   overrides = self: super: {
-  #     # If you need a particular version of a package instead
-  #     # of the default that nixpkgs provides, specify it here.
-  #   };
-  # };
+  haskell = haskellPackages.ghcWithPackages (p: map (name: p.${name}) [
+    "aeson"
+    "aeson-casing"
+    "containers"
+    "heroku-persistent"
+    "http-api-data"
+    "lucid"
+    "monad-logger"
+    "monad-control"
+    "mtl"
+    "persistent"
+    "persistent-template"
+    "persistent-postgresql"
+    "servant"
+    "servant-lucid"
+    "servant-server"
+    "text"
+    "time"
+    "transformers"
+    "wai"
+    "wai-cors"
+    "warp"
+    "yaml"
+     # etc., the rest of the Hackage packages you use
+  ]);
+
+
+  ghcVersion = "ghc865";
 
   ghcide =
     let 
@@ -16,9 +43,19 @@ let
     in
       (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
 
-in pkgs.stdenv.mkDerivation {
-    name = "env";
-    buildInputs = [
+in
+  pkgs.mkShell {
+      buildInputs = [
+        haskell
+        haskellPackages.ghcid
         ghcide
-    ];
-}
+      ];
+    LC_ALL = "en_US.UTF-8";
+    # LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    shellHook = ''
+      export NIX_GHC="${haskell}/bin/ghc"
+      export NIX_GHCPKG="${haskell}/bin/ghc-pkg"
+      export NIX_GHC_DOCDIR="${haskell}/share/doc/ghc/html"
+      export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+    '';
+  }
